0ba3e6dbbae2cc2db0e21e3bd1e2fbed22d45118
Kujivo2 <safchomm@gmail.com>
2026-05-10 14:54:24 +0200

    5

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,8 +4,8 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
-
+exports.REMOTE_DISTRO_URL = 'http://localhost:8080/docs/sample_distribution.json'
+console.log('[DISTRO URL]', exports.REMOTE_DISTRO_URL)
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
     null, // Injected forcefully by the preloader.
